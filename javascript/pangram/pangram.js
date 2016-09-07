c32b7b150e7927ef0af7bbf5f8c7a99da1f38f25
Declan <declan@alphero.com>
2016-09-08 11:57:03 +1200

    Update answer for pangram, change REGEX to ignore case so no longer need toLowerCase()

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,10 +2,10 @@ var Sentence = function(string) {
   this.sentence = string;
 };
 
-var REGEX = /([a-z])(?!.*\1)/g;
+var REGEX = /([a-z])(?!.*\1)/gi;
 
 Sentence.prototype.isPangram = function() {
-  var sentence = this.sentence.toLowerCase();
+  var sentence = this.sentence;
   return (sentence.match(REGEX) || []).length === 26;
 };
 
