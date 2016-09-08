9549e81d38231773adfdfd272168fba9ff8eebb7
Declan <declan@alphero.com>
2016-09-08 15:07:53 +1200

    solution for phone-number problem

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -2,8 +2,29 @@ var Phone = function(rawNumber) {
   this.rawNumber = rawNumber;
 };
 
+var REGEX = /\D+/g;
+var FAILED_STRING = '0000000000';
+
 Phone.prototype.number = function () {
-  return this.rawNumber;
+  var filteredString = this.rawNumber.replace(REGEX, '');
+
+  if(filteredString.length < 10 || (filteredString.length === 11 && filteredString[0] !== '1') || filteredString.length > 11) {
+    return FAILED_STRING;
+  }
+
+  return filteredString.substr(filteredString.length - 10);
+};
+
+Phone.prototype.areaCode = function() {
+  return this.rawNumber.substr(0, 3);
+};
+
+Phone.prototype.toString = function() {
+  var areaCode = this.rawNumber.substr(0, 3);
+  var localCode = this.rawNumber.substr(3, 3);
+  var number = this.rawNumber.substr(6);
+
+  return '(' + areaCode + ') ' + localCode + '-' + number;
 };
 
 module.exports = Phone;
